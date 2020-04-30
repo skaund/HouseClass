c06108a3a980709a4401d69e140d127b5ddfc5e4
Danil Vasilenko <danyav2@yandex.ru>
2020-04-30 18:58:26 +0400

    Добавьте файлы проекта.

---
+++ HouseClass/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseClass
+{
+    class House
+    {
+        public double price { get; set; }
+        public int age { get; set; }
+        public string place { get; set; }
+        public string name { get; set; }
+
+        public void GetHouseInfo()
+        {
+            Console.WriteLine($"Адрес: {name}| |Возраст: {age}| |Место: {place}|");
+        }
+    }
+
+
+    class House1 : House
+    {
+        public string type { get; set; }
+
+        public virtual void GetHouse1Info()
+        {
+            Console.WriteLine($"Адрес: {name}| |Возраст: {age}| |Место: {place}| |Цена дома: {price}| |Тип: {type}");
+        }
+    }
+}
---
+++ HouseClass/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            House house = new House();
+            house.name = "Шотландская вислоухая";
+            house.price = 6000;
+            house.age = 2;
+            house.place = "Китай";
+
+            house.GetHouseInfo();
+
+            House1 house1 = new House1();
+            house1.type = "Семейство кошачих";
+            house1.name = "Русская голубая";
+            house1.place = "Россия";
+            house1.price = 100;
+            house1.age = 5;
+
+
+            house1.GetHouse1Info();
+
+            Console.ReadLine();
+        }
+    }
+}
